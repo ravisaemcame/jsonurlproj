5bd1a1caf51b1727ad065f24dc0c1cc3cc2931e8
DELL <DELL@DESKTOP-DLCONCI>
2023-12-14 19:29:15 +0530

    initial commit

---
+++ main/JSONURLPROJ-1/src/main/java/com/example/demo1/Jsonurlproj1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jsonurlproj1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jsonurlproj1Application.class, args);
+	}
+
+}
